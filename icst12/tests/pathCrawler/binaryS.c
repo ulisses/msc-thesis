7a22b87da05092a93215f4bcf23d1395e6202ac5
Ulisses Costa <ulissesmonhecosta@gmail.com>
2011-11-04 14:46:31 +0000

    moved pathCrawler tests, changed proposal (prh changes) and icst abstract

--- icst12/tests/pathCrawler/binaryS.c
+++ icst12/tests/pathCrawler/binaryS.c
@@ -6,7 +6,7 @@ int BinarySearch(int x, int lo, int hi) {
 	while (lo < hi) {
 		int mid = (lo+hi)/2;
 		pathcrawler_assert(mid >= lo && mid < hi);
-		if (x < (mid >= mid)) { hi = mid; } else { lo = mid+1; }
+		if (x < mid) { hi = mid; } else { lo = mid+1; }
 	}
 	return lo; 
 }
